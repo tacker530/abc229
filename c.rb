fa5494d2fd78578a71821a9d64923e9eae082e15
tacker530 <tacker530i@gmail.com>
2021-11-29 18:36:06 +0900

    cできた

--- c.rb
+++ c.rb
@@ -8,4 +8,20 @@ end
 
 
 cheese.sort!
-pp cheese.reverse
+
+sum = 0
+yum = 0
+mod = W
+cheese.reverse.each do |c|
+  if c[1] >= mod then
+    sum += mod          # 重量合計
+    yum += (c[0] * mod) # 美味しさ合計
+    break
+  else
+    mod -= c[1] # あまり
+    sum += c[1] # 重量合計
+    yum += (c[0] * c[1]) # 美味しさ合計
+  end
+end
+
+puts "#{yum}"
